3e5badf944b4b202d076ca68cc3fb27f2086c6f6
Paweł Brzuszkiewicz <106952246+PABixn@users.noreply.github.com>
2024-10-03 22:29:36 +0200

    fixes

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in-up-on-scroll");
-const elementsToFadeInUpOnEnter = document.querySelectorAll(".fade-in-up-on-enter");
 
-// Function to check if the element is in the viewport and trigger animation
-function triggerFadeInUp(element) {
-  if (!element.classList.contains("fade-in-up") && window.scrollY >= (element.offsetTop - window.innerHeight)) {
-    element.classList.add("fade-in-up");
-  }
-}
-
-// Scroll event listener for fade-in-up-on-scroll elements
-if (elementsToFadeInUpOnScroll) {
-  window.addEventListener("scroll", function(event) {
-    elementsToFadeInUpOnScroll.forEach(function(element) {
-      triggerFadeInUp(element);
-    });
+function triggerFadeInUp(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fade-in-up");
+      observer.unobserve(entry.target);
+    }
   });
 }
 
-// Load event listener for fade-in-up-on-enter elements
-if (elementsToFadeInUpOnEnter) {
-  window.addEventListener("load", function(event) {
-    elementsToFadeInUpOnEnter.forEach(function(element) {
-      triggerFadeInUp(element);
-    });
-  });
-}
+
+const observer = new IntersectionObserver(triggerFadeInUp, {
+  threshold: 0.01
+});
+
+elementsToFadeInUpOnScroll.forEach(element => {
+  observer.observe(element);
+});
+
+elementsToFadeInUpOnEnter.forEach(element => {
+  observer.observe(element);
+});
